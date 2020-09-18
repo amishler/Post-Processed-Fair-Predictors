b7599be3eec64e36d769481ae74acebcae4c2193
Alan Mishler <contact@alanmishler.com>
2020-09-18 05:12:00 -0500

    Clean up tests for estimation functions

--- tests/test_functions_estimation.py
+++ tests/test_functions_estimation.py
@@ -1,26 +1,20 @@
+import pytest
+
 import numpy as np
 import pandas as pd
-from pathlib import Path
-import pytest
+
 from sklearn.linear_model import LogisticRegression
 
-from counterfactualEO import functions_estimation as fe
-
-#############################################
-#### Fixtures for synthetic & saved data ####
-#############################################
-
-@pytest.fixture
-def mock_data():
-    n = 100
-    return pd.DataFrame({
-        'A': np.random.randint(0, 2, size=n),
-        'X1': np.random.randn(n),
-        'X2': np.random.randn(n),
-        'R': np.random.randint(0, 2, size=n),
-        'D': np.random.randint(0, 2, size=n),
-        'Y': np.random.randint(0, 2, size=n)
-    })
+from counterfactualEO.functions_estimation import (
+    train_nuisance,
+    risk_coef,
+    risk_coefs,
+    est_cFPR_overall,
+    est_cFNR_overall,
+    fairness_coefs,
+    optimize,
+    fair_derived
+)
 
 
 ##############################
@@ -30,7 +24,7 @@ def mock_data():
 def test_train_nuisance(mock_data):
     X = ['X1', 'X2']
     model = LogisticRegression()
-    out = fe.train_nuisance(mock_data.iloc[:50], mock_data.iloc[50:], A='A', X=X, R='R', D='D', Y='Y',
+    out = train_nuisance(mock_data.iloc[:50], mock_data.iloc[50:], A='A', X=X, R='R', D='D', Y='Y',
                             learner_pi=model, learner_mu=model)
     assert isinstance(out, pd.DataFrame)
     assert set(out.columns) == {'pihat', 'muhat0', 'phihat'}
@@ -38,25 +32,25 @@ def test_train_nuisance(mock_data):
 
 def test_risk_coef_and_risk_coefs(mock_data):
     mock_data['phihat'] = np.random.rand(len(mock_data))
-    single = fe.risk_coef(mock_data, 0, 1, outcome='phihat')
+    single = risk_coef(mock_data, 0, 1, outcome='phihat')
     assert isinstance(single, float)
 
-    coefs = fe.risk_coefs(mock_data, outcome='phihat')
+    coefs = risk_coefs(mock_data, outcome='phihat')
     assert isinstance(coefs, np.ndarray)
     assert coefs.shape == (4,)
 
 
-def test_cFPR_and_cFNR(mock_data):
+def test_est_cFPR_and_cFNR_overall(mock_data):
     mock_data['phihat'] = np.clip(np.random.rand(len(mock_data)), 0.01, 0.99)
-    fpr = fe.cFPR(mock_data, R='R', outcome='phihat')
-    fnr = fe.cFNR(mock_data, R='R', outcome='phihat')
+    fpr = est_cFPR_overall(mock_data, R='R', outcome='phihat')
+    fnr = est_cFNR_overall(mock_data, R='R', outcome='phihat')
     assert 0 <= fpr <= 1
     assert 0 <= fnr <= 1
 
 
 def test_fairness_coefs(mock_data):
     mock_data['phihat'] = np.clip(np.random.rand(len(mock_data)), 0.01, 0.99)
-    pos, neg = fe.fairness_coefs(mock_data, A='A', R='R', outcome='phihat')
+    pos, neg = fairness_coefs(mock_data, A='A', R='R', outcome='phihat')
     assert isinstance(pos, np.ndarray)
     assert pos.shape == (4,)
     assert isinstance(neg, np.ndarray)
@@ -67,7 +61,7 @@ def test_optimize():
     risk = np.array([0.1, 0.2, 0.3, 0.4])
     coefs_pos = np.array([1, -1, 0, 0])
     coefs_neg = np.array([0, 0, 1, -1])
-    theta = fe.optimize(risk, coefs_pos, coefs_neg, epsilon_pos=0.1, epsilon_neg=0.1)
+    theta = optimize(risk, coefs_pos, coefs_neg, epsilon_pos=0.1, epsilon_neg=0.1)
     assert isinstance(theta, np.ndarray)
     assert theta.shape == (4,)
     assert np.all((0 <= theta) & (theta <= 1))
@@ -76,7 +70,7 @@ def test_optimize():
 def test_fair_derived(mock_data):
     X = ['X1', 'X2']
     model = LogisticRegression()
-    result = fe.fair_derived(data=mock_data, A='A', X=X, R='R', D='D', Y='Y',
+    result = fair_derived(data=mock_data, A='A', X=X, R='R', D='D', Y='Y',
                              learner_pi=model, learner_mu=model,
                              epsilon_pos=0.1, epsilon_neg=0.1)
     assert isinstance(result, dict)
@@ -91,40 +85,14 @@ def test_fair_derived(mock_data):
 @pytest.mark.parametrize("outcome_key", ['phihat', 'mu0'])
 def test_risk_coefs_real_data(real_data, outcome_key):
     df, testdict = real_data
-    result = fe.risk_coefs(df, A='A', R='R', outcome=outcome_key)
+    result = risk_coefs(df, A='A', R='R', outcome=outcome_key)
     expected = testdict[f'risk_coefs_{outcome_key}']
-    np.testing.assert_allclose(result, expected)
+    np.testing.assert_array_almost_equal(result, expected)
 
 
 @pytest.mark.parametrize("outcome_key", ['phihat', 'mu0'])
 def test_fairness_coefs_real_data(real_data, outcome_key):
     df, testdict = real_data
-    pos, neg = fe.fairness_coefs(df, outcome='phihat' if outcome_key == 'phihat' else 'mu0')
-    np.testing.assert_allclose(pos, testdict[f'coefs_pos_{outcome_key}'])
-    np.testing.assert_allclose(neg, testdict[f'coefs_neg_{outcome_key}'])
-
-@pytest.mark.parametrize("ci_scale", ['expit', 'logit'])
-def test_est_risk(real_data, ci_scale):
-    df, testdict = real_data
-    theta = testdict['theta']
-    result = fe.est_risk(theta, df, outcome='phihat', ci=0.95, ci_scale=ci_scale)
-    expected = testdict['risk_df'] if ci_scale == 'expit' else testdict['risk_df_logit']
-    pd.testing.assert_frame_equal(result, expected)
-
-
-@pytest.mark.parametrize("ci_scale", ['expit', 'logit'])
-def test_est_cFPR(real_data, ci_scale):
-    df, testdict = real_data
-    theta = testdict['theta']
-    result = fe.est_cFPR(theta, df, outcome='phihat', ci=0.95, ci_scale=ci_scale)
-    expected = testdict['cFPR_df'] if ci_scale == 'expit' else testdict['cFPR_df_logit']
-    pd.testing.assert_frame_equal(result, expected)
-
-
-@pytest.mark.parametrize("ci_scale", ['expit', 'logit'])
-def test_est_cFNR(real_data, ci_scale):
-    df, testdict = real_data
-    theta = testdict['theta']
-    result = fe.est_cFNR(theta, df, outcome='phihat', ci=0.95, ci_scale=ci_scale)
-    expected = testdict['cFNR_df'] if ci_scale == 'expit' else testdict['cFNR_df_logit']
-    pd.testing.assert_frame_equal(result, expected)
+    pos, neg = fairness_coefs(df, outcome='phihat' if outcome_key == 'phihat' else 'mu0')
+    np.testing.assert_array_almost_equal(pos, testdict[f'coefs_pos_{outcome_key}'])
+    np.testing.assert_array_almost_equal(neg, testdict[f'coefs_neg_{outcome_key}'])
