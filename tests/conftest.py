37e2bb1def172cbb667d6c034b2067a89ad3c4d9
Alan Mishler <contact@alanmishler.com>
2025-04-01 21:22:51 -0400

    Add test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
-# tests/conftest.py
 from pathlib import Path
 import pytest
+
+import numpy as np
 import pandas as pd
 import pickle
 
+
+@pytest.fixture
+def mock_data():
+    n = 100
+    return pd.DataFrame({
+        'A': np.random.randint(0, 2, size=n),
+        'X1': np.random.randn(n),
+        'X2': np.random.randn(n),
+        'R': np.random.randint(0, 2, size=n),
+        'D': np.random.randint(0, 2, size=n),
+        'Y': np.random.randint(0, 2, size=n)
+    })
+
+
 @pytest.fixture(scope="session")
 def real_data():
-    base_path = Path(__file__).parent
+    base_path = Path(__file__).parent  # this resolves to tests/
     df = pd.read_csv(base_path / "test_data.csv")
     with open(base_path / "test_values1.pickle", "rb") as f:
         test_values = pickle.load(f)
